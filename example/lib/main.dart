47cc3f38e4436e15d499cd36ed24cabdb9e60de0
unknown <abheet.gera01@gmail.com>
2020-05-10 19:08:42 +0530

    Add all my files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,13 +4,68 @@ import 'package:screen_keep_on/screen_keep_on.dart';
 
 void main() => runApp(MyApp());
 
+
+
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
+var link;
+bool valueg = false;
 class _MyAppState extends State<MyApp> {
   final controller = RtmpLiveViewController();
+
+  var _hint=" 192.168.0.111 ";
+  TextEditingController _textFieldController = TextEditingController();
+
+  void _onChanged(bool value){
+    setState(() {
+     valueg=value;
+     if(valueg==true){
+     showDialog(context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text(
+            "Enter the IP of your network",
+            textScaleFactor: 0.9,
+            style: TextStyle(fontWeight: FontWeight.w300, fontFamily: "Roboto", color: Colors.white),
+            ),
+          content: Container(
+            color: Colors.white,
+            child: TextField(
+                autofocus: true,
+                onTap: (){_hint="";},
+                controller: _textFieldController,
+                decoration: InputDecoration(hintText: _hint),
+                style: TextStyle(fontWeight: FontWeight.w300, fontFamily: "Roboto", color: Colors.black),
+              ),
+          ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('SUBMIT'),
+                onPressed: () {
+                    _hint=" 192.168.0.111 ";
+                    if(_textFieldController.text!="")
+                      link=_textFieldController.text;
+                    Navigator.of(context).pop();
+                   }
+                ),
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }, 
+              )
+            ],
+          backgroundColor: Colors.black,
+          );
+      }
+     );} 
+    });
+  }
+
 
   @override
   void initState() {
@@ -34,7 +89,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Live Stream app'),
           actions: <Widget>[
             ValueListenableBuilder<RtmpStatus>(
               valueListenable: controller.status,
@@ -42,7 +97,7 @@ class _MyAppState extends State<MyApp> {
                 icon: Icon(status?.isStreaming == true ? Icons.cast_connected : Icons.cast),
                 onPressed: () {
                   if (!status.isStreaming)
-                    controller.connect(rtmpUrl: 'rtmp://dev.cuminas.jp:1935/hls', streamName: 'test');
+                    controller.connect(rtmpUrl: 'rtmp://'+link+':2222/live/app', streamName: 'live');
                   else
                     controller.disconnect();
                 }
@@ -56,7 +111,8 @@ class _MyAppState extends State<MyApp> {
                   controller.swapCamera();
                 }
               )
-            )
+            ),
+            Switch(value:valueg , onChanged: (bool value) {_onChanged(value);},)
           ],
         ),
         body: Container(
